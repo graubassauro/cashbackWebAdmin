a0aec767c7154db3d9e75fc9cf44b9f6a787e359
thereallucas98 <lucas1998david@gmail.com>
2023-06-20 07:12:31 -0300

    [CHORE]: project's setup

--- src/app/directives/alerts/deleteAlert/delete-item.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DeleteItemDirective } from './delete-item.directive';
-
-describe('DeleteItemDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DeleteItemDirective();
-    expect(directive).toBeTruthy();
-  });
-});
